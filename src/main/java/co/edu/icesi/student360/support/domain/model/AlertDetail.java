dd1fd3081c203af3f6500364f3ffd3171975270a
Kevin Steven Nieto Curaca <kevin.nieto@u.icesi.edu.co>
2026-08-30 11:40:41 -0500

    refactor(domain): remove the service classes superseded by cqrs handlers
    AlertService and WellbeingService are replaced by the command and query handlers of the previous commit; AlertDetail is replaced by the AlertDetailView read model.

--- src/main/java/co/edu/icesi/student360/support/domain/model/AlertDetail.java
+++
@@ -1,7 +0,0 @@
-package co.edu.icesi.student360.support.domain.model;
-
-import java.util.List;
-import java.util.Optional;
-
-public record AlertDetail(
-    Alert alert, Optional<InterventionPlan> plan, List<SupportReport> reports) {}
